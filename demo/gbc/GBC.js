79989d63c98c38ed54a171231c03733a9b567180
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-04-15 14:15:47 -0700

    ftyguhjohgjklhgc

--- demo/gbc/GBC.js
+++ demo/gbc/GBC.js
@@ -7,11 +7,8 @@ cpu.addRegister(8); // Register C [2]
 cpu.addRegister(8); // Register D [3]
 cpu.addRegister(8); // Register E [4]
 cpu.addRegister(8); // Register F [5]
-cpu.addRegister(8); // Register G [6]
-cpu.addRegister(8); // Register H [7]
-
-// system registers
-cpu.addRegister(8) // SYSTEM REGISTER [8]
+cpu.addRegister(8); // Register H [6]
+cpu.addRegister(8); // Register L [7]
 
 // Load BC,d16
 cpu.setOpcode(0x01, function (Opcode) {
@@ -71,4 +68,9 @@ cpu.setOpcode(0x06, function (Opcode) {
     Opcode.SetComebackFunction(function (Comeback0) {
         Comeback0.Registers[1].data = Comeback0.Data;
     });
+});
+
+// Rotate Left Circular Accumulator
+cpu.setOpcode(0x07, function (Opcode) {
+    //
 });
