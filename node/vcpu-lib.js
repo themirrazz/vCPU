51ccbbc3985f9be30133a1723ddf154ac62a1d99
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-04-15 12:52:31 -0700

    ぼくはほんとにばかやろうですね？

--- node/vcpu-lib.js
+++ node/vcpu-lib.js
@@ -172,7 +172,7 @@ modules.export = (function () {
                     }
                     return arr;
                 },
-                data: $value;
+                data: $value
             });
         } catch (error) {
             // the manufacturing company did a bad job
